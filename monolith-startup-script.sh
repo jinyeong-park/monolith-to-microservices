e8041e2c278b17b4d3ed3695ac25dfab27b7a1cf
Jinyeong <jinyeongpark1205@gamil.com>
2025-05-25 20:55:15 -0700

    update monolit-startup-script.sh for vm

--- monolith-startup-script.sh
+++ monolith-startup-script.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
 
+
 # Store PROJECT_ID from metadata server for later use
 export PROJECT_ID=$(curl -s "http://metadata.google.internal/computeMetadata/v1/project/project-id" -H "Metadata-Flavor: Google")
+
+
+
+
+# 하드코딩된 버킷 이름 (PROJECT_ID와 별개)
+BUCKET_NAME="vm-project-460422"
+
 
 # Install logging monitor
 curl -s "https://storage.googleapis.com/signals-agents/logging/google-fluentd-install.sh" | bash
 service google-fluentd restart &
 
+
 # Install dependencies from apt
 apt-get update
 apt-get install -yq ca-certificates git build-essential supervisor psmisc
+
 
 # Install nodejs
 mkdir -p /opt/nodejs
@@ -17,22 +27,26 @@ curl https://nodejs.org/dist/v16.14.0/node-v16.14.0-linux-x64.tar.gz | tar xvzf 
 ln -sf /opt/nodejs/bin/node /usr/bin/node
 ln -sf /opt/nodejs/bin/npm /usr/bin/npm
 
-# Get the application source code from the Google Cloud Storage bucket
-mkdir -p /monolith-app
-gsutil -m cp -r gs://monolith-store-$PROJECT_ID/monolith-to-microservices/monolith/* /monolith-app/
+
+# Get the application source code from the specified Google Cloud Storage bucket
+mkdir /fancy-store
+gsutil -m cp -r gs://$BUCKET_NAME/monolith-to-microservices/monolith/* /fancy-store/
+
 
 # Install app dependencies
-cd /monolith-app/
+cd /fancy-store/
 npm install
+
 
 # Create a nodeapp user. The application will run as this user
 useradd -m -d /home/nodeapp nodeapp
-chown -R nodeapp:nodeapp /monolith-app
+chown -R nodeapp:nodeapp /opt/app
+
 
 # Configure supervisor to run the node app
 cat >/etc/supervisor/conf.d/node-app.conf << EOF
 [program:nodeapp]
-directory=/monolith-app
+directory=/fancy-store
 command=npm start
 autostart=true
 autorestart=true
@@ -42,12 +56,15 @@ stdout_logfile=syslog
 stderr_logfile=syslog
 EOF
 
+
 # Make sure supervisord is running
 service supervisor start
+
 
 # Use supervisorctl without sudo (it should run as root already in startup script)
 supervisorctl reread
 supervisorctl update
+
 
 # Output status for logging
 echo "Startup script completed, checking supervisor status:"
