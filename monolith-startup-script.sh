075320427df5774044bd02df8b086b8c2a118308
Jinyeong <jinyeongpark1205@gamil.com>
2025-05-25 21:09:22 -0700

    update monolit-startup-script.sh for vm2

--- monolith-startup-script.sh
+++ monolith-startup-script.sh
@@ -5,12 +5,6 @@
 export PROJECT_ID=$(curl -s "http://metadata.google.internal/computeMetadata/v1/project/project-id" -H "Metadata-Flavor: Google")
 
 
-
-
-# 하드코딩된 버킷 이름 (PROJECT_ID와 별개)
-BUCKET_NAME="vm-project-460422"
-
-
 # Install logging monitor
 curl -s "https://storage.googleapis.com/signals-agents/logging/google-fluentd-install.sh" | bash
 service google-fluentd restart &
@@ -28,25 +22,25 @@ ln -sf /opt/nodejs/bin/node /usr/bin/node
 ln -sf /opt/nodejs/bin/npm /usr/bin/npm
 
 
-# Get the application source code from the specified Google Cloud Storage bucket
-mkdir /fancy-store
-gsutil -m cp -r gs://$BUCKET_NAME/monolith-to-microservices/monolith/* /fancy-store/
+# Get the application source code from the Google Cloud Storage bucket
+mkdir -p /monolith-app
+gsutil -m cp -r gs://monolith-store-$PROJECT_ID/monolith-to-microservices/monolith/* /monolith-app/
 
 
 # Install app dependencies
-cd /fancy-store/
+cd /monolith-app/
 npm install
 
 
 # Create a nodeapp user. The application will run as this user
 useradd -m -d /home/nodeapp nodeapp
-chown -R nodeapp:nodeapp /opt/app
+chown -R nodeapp:nodeapp /monolith-app
 
 
 # Configure supervisor to run the node app
 cat >/etc/supervisor/conf.d/node-app.conf << EOF
 [program:nodeapp]
-directory=/fancy-store
+directory=/monolith-app
 command=npm start
 autostart=true
 autorestart=true
